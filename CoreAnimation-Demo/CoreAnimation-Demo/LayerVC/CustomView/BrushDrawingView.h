21217a592caf50b4e02689a019f8ad85fc083796
luowei <745054025@qq.com>
2015-04-16 18:29:28 +0800

    添加了一些绘图代码...

---
+++ CoreAnimation-Demo/CoreAnimation-Demo/LayerVC/CustomView/BrushDrawingView.h
@@ -0,0 +1,13 @@
+//
+//  DrawingView.h
+//  CoreAnimation-Demo
+//
+//  Created by luowei on 15/4/15.
+//  Copyright (c) 2015年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrushDrawingView : UIView
+
+@end
---
+++ CoreAnimation-Demo/CoreAnimation-Demo/LayerVC/CustomView/BrushDrawingView.m
@@ -0,0 +1,87 @@
+//
+//  DrawingView.m
+//  CoreAnimation-Demo
+//
+//  Created by luowei on 15/4/15.
+//  Copyright (c) 2015年 luowei. All rights reserved.
+//
+
+#import "BrushDrawingView.h"
+
+#define BRUSH_SIZE 32
+
+@interface BrushDrawingView ()
+@property(nonatomic, strong) NSMutableArray *strokes;
+@end
+
+@implementation BrushDrawingView
+
+- (void)awakeFromNib {
+    //create array
+    self.strokes = [NSMutableArray array];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    //get the starting point
+    CGPoint point = [[touches anyObject] locationInView:self];
+    //add brush stroke
+    [self addBrushStrokeAtPoint:point];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    //get the touch point
+    CGPoint point = [[touches anyObject] locationInView:self];
+    //add brush stroke
+    [self addBrushStrokeAtPoint:point];
+}
+
+/*
+- (void)addBrushStrokeAtPoint:(CGPoint)point
+{
+    //add brush stroke to array
+    [self.strokes addObject:[NSValue valueWithCGPoint:point]];
+    //needs redraw
+    [self setNeedsDisplay];
+}
+- (void)drawRect:(CGRect)rect
+{
+    //redraw strokes
+    for (NSValue *value in self.strokes) {
+        //get point
+        CGPoint point = [value CGPointValue];
+        //get brush rect
+        CGRect brushRect = CGRectMake(point.x - BRUSH_SIZE/2, point.y - BRUSH_SIZE/2, BRUSH_SIZE, BRUSH_SIZE);
+        //draw brush stroke    ?
+        [[UIImage imageNamed:@"mm.jpg"] drawInRect:brushRect];
+    }
+}
+*/
+
+- (void)addBrushStrokeAtPoint:(CGPoint)point {
+    //add brush stroke to array
+    [self.strokes addObject:[NSValue valueWithCGPoint:point]];
+    //set dirty rect
+    [self setNeedsDisplayInRect:[self brushRectForPoint:point]];
+}
+
+- (CGRect)brushRectForPoint:(CGPoint)point {
+    return CGRectMake(point.x - BRUSH_SIZE / 2, point.y - BRUSH_SIZE / 2, BRUSH_SIZE, BRUSH_SIZE);
+}
+
+- (void)drawRect:(CGRect)rect {
+    //redraw strokes
+    for (NSValue *value in self.strokes) {
+        //get point
+        CGPoint point = [value CGPointValue];
+        //get brush rect
+        CGRect brushRect = [self brushRectForPoint:point];
+
+        //only draw brush stroke if it intersects dirty rect
+        if (CGRectIntersectsRect(rect, brushRect)) {
+            //draw brush stroke
+            [[UIImage imageNamed:@"mm.jpg"] drawInRect:brushRect];
+        }
+    }
+}
+
+@end
